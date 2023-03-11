07f2add9ded8526a723f9b861cc15ae494ed61d4
Orlando Canchola Jimenez <orcj17544@gmail.com>
2023-03-11 02:15:38 -0600

    The recording already works

--- app/src/main/java/com/example/android/fluentspeak/ConversationFragment.kt
+++ app/src/main/java/com/example/android/fluentspeak/ConversationFragment.kt
@@ -1,24 +1,48 @@
 package com.example.android.fluentspeak
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.media.MediaRecorder
+import android.os.Build
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
 import com.example.android.fluentspeak.databinding.FragmentConversationBinding
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.snackbar.Snackbar
+import java.io.File
+import java.io.IOException
 import java.util.*
+
+enum class RECORDING_STATE {
+    START, PAUSE, STOP
+}
+
+enum class TRANSLATING_STATE {
+    START, STOP
+}
 
 class ConversationFragment : Fragment() {
 
     private lateinit var viewModel: ConversationViewModel
 
-    lateinit var textToSpeech: TextToSpeech
+    private var currentRecordingState = RECORDING_STATE.STOP
+    private var currentTranslatingState = TRANSLATING_STATE.STOP
+
+    private lateinit var recorder: MediaRecorder
+    private lateinit var translator: MediaRecorder
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,45 +54,181 @@ class ConversationFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(ConversationViewModel::class.java)
 
         binding.recordButton.setOnClickListener {
-
-        }
-
-
-        textToSpeech = TextToSpeech(this.context, TextToSpeech.OnInitListener { status ->
-            if (status == TextToSpeech.SUCCESS) {
-                textToSpeech.setLanguage(Locale.US)
-            } else {
-                Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show()
+            when(currentRecordingState) {
+                RECORDING_STATE.START -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    recorder.pause()
+                    (it as MaterialButton).icon =
+                        resources.getDrawable(R.drawable.baseline_play_arrow_24, null)
+                    binding.translateButton.setEnabled(true)
+                    currentRecordingState = RECORDING_STATE.PAUSE
+                }
+                RECORDING_STATE.STOP -> {
+                    recorder.start()
+                    (it as MaterialButton).icon = resources.getDrawable(R.drawable.baseline_pause_24, null)
+                    binding.stopButton.setEnabled(true)
+                    binding.translateButton.setEnabled(false)
+                    currentRecordingState = RECORDING_STATE.START
+                }
+                RECORDING_STATE.PAUSE -> {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                        recorder.resume()
+                    }
+                    (it as MaterialButton).icon = resources.getDrawable(R.drawable.baseline_pause_24, null)
+                    binding.translateButton.setEnabled(false)
+                    currentRecordingState = RECORDING_STATE.START
+                }
             }
-        })
-
-        /*
-
-        val speak: Button = view.findViewById(R.id.speak)
-
-        speak.setOnClickListener {
-            textToSpeech.speak("Hello, how are you?", TextToSpeech.QUEUE_FLUSH, null)
         }
 
-        val chatbot: Button = view.findViewById(R.id.chatbot)
-        val chatBotResponse: TextView = view.findViewById(R.id.chatbot_response)
-
-        chatbot.setOnClickListener {
-            viewModel.getChatBotResponse().observe(viewLifecycleOwner, { response ->
-                chatBotResponse.text = response
-            })
+        binding.stopButton.setOnClickListener {
+            when(currentRecordingState) {
+                RECORDING_STATE.START, RECORDING_STATE.PAUSE -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    recorder.stop()
+                    configureRecorder()
+                    it.setEnabled(false)
+                    (binding.recordButton as MaterialButton).icon = resources.getDrawable(R.drawable.baseline_mic_24, null)
+                    binding.translateButton.setEnabled(true)
+                    currentRecordingState = RECORDING_STATE.STOP
+                }
+                RECORDING_STATE.STOP -> return@setOnClickListener
+            }
         }
 
-        val whisper: Button = view.findViewById(R.id.whisper)
-        val whisperResponse: TextView = view.findViewById(R.id.whisper_response)
-
-        whisper.setOnClickListener {
-
+        binding.translateButton.setOnClickListener {
+            when(currentRecordingState) {
+                RECORDING_STATE.START -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    return@setOnClickListener
+                }
+                RECORDING_STATE.STOP -> {
+                    when(currentTranslatingState) {
+                        TRANSLATING_STATE.STOP -> {
+                            translator.start()
+                            (binding.translateButton as MaterialButton).icon = resources.getDrawable(R.drawable.baseline_stop_24, null)
+                            binding.recordButton.setEnabled(false)
+                            binding.stopButton.setEnabled(false)
+                            currentTranslatingState = TRANSLATING_STATE.START
+                        }
+                        TRANSLATING_STATE.START -> {
+                            translator.stop()
+                            configureTranslator()
+                            (binding.translateButton as MaterialButton).icon = resources.getDrawable(R.drawable.baseline_translate_24, null)
+                            binding.recordButton.setEnabled(true)
+                            currentTranslatingState = TRANSLATING_STATE.STOP
+                        }
+                    }
+                }
+                RECORDING_STATE.PAUSE -> {
+                    when(currentTranslatingState) {
+                        TRANSLATING_STATE.STOP -> {
+                            translator.start()
+                            (binding.translateButton as MaterialButton).icon = resources.getDrawable(R.drawable.baseline_stop_24, null)
+                            binding.recordButton.setEnabled(false)
+                            binding.stopButton.setEnabled(false)
+                            currentTranslatingState = TRANSLATING_STATE.START
+                        }
+                        TRANSLATING_STATE.START -> {
+                            translator.stop()
+                            configureTranslator()
+                            (binding.translateButton as MaterialButton).icon = resources.getDrawable(R.drawable.baseline_translate_24, null)
+                            binding.recordButton.setEnabled(true)
+                            binding.stopButton.setEnabled(true)
+                            currentTranslatingState = TRANSLATING_STATE.STOP
+                        }
+                    }
+                }
+            }
         }
 
-
-         */
 
         return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    // Permission is granted. Continue the action or workflow in your
+                    // app.
+                } else {
+                    // Explain to the user that the feature is unavailable because the
+                    // feature requires a permission that the user has denied. At the
+                    // same time, respect the user's decision. Don't link to system
+                    // settings in an effort to convince the user to change their
+                    // decision.
+                    Snackbar.make(this.requireView(), "Voice recording does not work without the permission", Snackbar.LENGTH_SHORT).show()
+                }
+            }
+
+        when {
+            ContextCompat.checkSelfPermission(
+                this.requireContext(),
+                android.Manifest.permission.RECORD_AUDIO
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                // You can use the API that requires the permission.
+            }
+            shouldShowRequestPermissionRationale(android.Manifest.permission.RECORD_AUDIO) -> {
+                // In an educational UI, explain to the user why your app requires this
+                // permission for a specific feature to behave as expected, and what
+                // features are disabled if it's declined. In this UI, include a
+                // "cancel" or "no thanks" button that lets the user continue
+                // using your app without granting the permission.
+                Snackbar.make(this.requireView(), "Voice recording does not work without the permission", Snackbar.LENGTH_SHORT).show()
+            }
+            else -> {
+                // You can directly ask for the permission.
+                // The registered ActivityResultCallback gets the result of this request.
+                requestPermissionLauncher.launch(
+                    android.Manifest.permission.RECORD_AUDIO)
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        recorder = MediaRecorder()
+        translator = MediaRecorder()
+
+        configureRecorder()
+        configureTranslator()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        recorder.release()
+        translator.release()
+    }
+
+    private fun configureRecorder() {
+        recorder.apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT)
+
+            File.createTempFile("recording", null, context?.cacheDir)
+            val cacheFile = File(context?.cacheDir, "recording")
+
+            setOutputFile(cacheFile.absolutePath)
+
+            prepare()
+        }
+    }
+
+    private fun configureTranslator() {
+        translator.apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT)
+
+            File.createTempFile("translation", null, context?.cacheDir)
+            val cacheFile = File(context?.cacheDir, "translation")
+
+            setOutputFile(cacheFile.absolutePath)
+
+            prepare()
+        }
     }
 }
