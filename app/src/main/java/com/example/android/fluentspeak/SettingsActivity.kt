4a014239885a29f19f683999b7e0205fcf9a7957
Orlando Canchola Jimenez <orcj17544@gmail.com>
2023-04-11 15:33:28 -0600

    The toast already made in Settings Activity

--- app/src/main/java/com/example/android/fluentspeak/SettingsActivity.kt
+++ app/src/main/java/com/example/android/fluentspeak/SettingsActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.android.fluentspeak.databinding.ActivitySettingsBinding
 import com.example.android.fluentspeak.network.Voice
@@ -26,6 +27,42 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_settings)
 
+        binding.whisperTemperatureImage.setOnClickListener {
+            Toast.makeText(this, "The sampling temperature, between 0 and 1. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic. If set to 0, the model will use log probability to automatically increase the temperature until certain thresholds are hit.", Toast.LENGTH_LONG).show()
+        }
+
+        binding.chatGptTemperatureImage.setOnClickListener {
+            Toast.makeText(this, "What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.\n" +
+                    "\n" +
+                    "We generally recommend altering this or top_p but not both.", Toast.LENGTH_LONG).show()
+        }
+
+        binding.chatGptMaxTokensImage.setOnClickListener {
+            Toast.makeText(this, "The maximum number of tokens to generate in the chat completion.\n" +
+                    "\n" +
+                    "The total length of input tokens and generated tokens is limited by the model's context length.", Toast.LENGTH_LONG).show()
+        }
+
+        binding.chatGptPresencePenaltyImage.setOnClickListener {
+            Toast.makeText(this, "Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.", Toast.LENGTH_LONG).show()
+        }
+
+        binding.chatGptFrecuencyPenaltyImage.setOnClickListener {
+            Toast.makeText(this, "Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.", Toast.LENGTH_LONG).show()
+        }
+
+        binding.textToSpeechAccentImage.setOnClickListener {
+            Toast.makeText(this, "AU means Australian\nIN means Indian\nGB means Great Britain", Toast.LENGTH_LONG).show()
+        }
+
+        binding.textToSpeechGenderImage.setOnClickListener {
+            Toast.makeText(this, "Male voice or Female voice", Toast.LENGTH_LONG).show()
+        }
+
+        binding.textToSpeechVoiceNameImage.setOnClickListener {
+            Toast.makeText(this, "The voice name that match with the accent and gender previously selected", Toast.LENGTH_LONG).show()
+        }
+
         voiceNameField = binding.textToSpeechVoiceNameField
         accentField = binding.textToSpeechAccentField
         genderField = binding.textToSpeechGenderField
@@ -43,12 +80,6 @@ class SettingsActivity : AppCompatActivity() {
         (accentField.editText as AutoCompleteTextView).setOnItemClickListener { adapterView, view, position, id ->
             voiceNameAccentFilter = ((view as TextView).text as String)
 
-            when(voiceNameAccentFilter) {
-                accentItems[0] -> voiceNameAccentFilter = accentItems[0]
-                accentItems[1] -> voiceNameAccentFilter = accentItems[1]
-                accentItems[2] -> voiceNameAccentFilter = accentItems[2]
-            }
-
             val newVoiceItems = filterVoices()
 
             updateVoiceNameField(newVoiceItems.map { it.name })
@@ -56,12 +87,6 @@ class SettingsActivity : AppCompatActivity() {
 
         (genderField.editText as AutoCompleteTextView).setOnItemClickListener { adapterView, view, position, id ->
             voiceNameGenderFilter = ((view as TextView).text as String)
-
-            when(voiceNameGenderFilter) {
-                genderItems[0] -> voiceNameGenderFilter = genderItems[0]
-                genderItems[1] -> voiceNameGenderFilter = genderItems[1]
-                genderItems[2] -> voiceNameGenderFilter = genderItems[2]
-            }
 
             val newVoiceNameItems = filterVoices()
 
