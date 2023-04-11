4c7a603bf348e0dd8def410060032f38d00856fd
Orlando Canchola Jimenez <orcj17544@gmail.com>
2023-04-10 21:22:35 -0600

    The filters to Voices in settings works as expected

--- app/src/main/java/com/example/android/fluentspeak/SettingsActivity.kt
+++ app/src/main/java/com/example/android/fluentspeak/SettingsActivity.kt
@@ -7,6 +7,7 @@ import android.widget.AutoCompleteTextView
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.example.android.fluentspeak.databinding.ActivitySettingsBinding
+import com.example.android.fluentspeak.network.Voice
 import com.google.android.material.textfield.TextInputLayout
 import org.w3c.dom.Text
 
@@ -17,67 +18,54 @@ class SettingsActivity : AppCompatActivity() {
     lateinit var accentField: TextInputLayout
     lateinit var genderField: TextInputLayout
     lateinit var voiceNameField: TextInputLayout
+
+    var voiceNameAccentFilter = ""
+    var voiceNameGenderFilter = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_settings)
-        val binding = DataBindingUtil.setContentView<ActivitySettingsBinding>(this, R.layout.activity_settings)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_settings)
 
         voiceNameField = binding.textToSpeechVoiceNameField
         accentField = binding.textToSpeechAccentField
         genderField = binding.textToSpeechGenderField
 
-        var voiceNameAccentFilter = ""
-        var voiceNameGenderFilter = ""
-
-        val accentItems = TextToSpeechSettingsData.ACCENTS.map { it.value }
+        val accentItems = TextToSpeechSettingsData.VOICES.map { it.languageCode }.distinct()
         val accentAdapter = ArrayAdapter(this, R.layout.item, accentItems)
         (accentField.editText as? AutoCompleteTextView)?.setAdapter(accentAdapter)
 
-        val genderItems = TextToSpeechSettingsData.GENDERS.map { it.value }
+        val genderItems = TextToSpeechSettingsData.VOICES.map { it.ssmlGender }.distinct()
         val genderAdapter = ArrayAdapter(this, R.layout.item, genderItems)
         (genderField.editText as? AutoCompleteTextView)?.setAdapter(genderAdapter)
 
-        val voiceNameItems = TextToSpeechSettingsData.VOICE_NAMES.map { it.value }
-        updateVoiceNameField(voiceNameItems)
+        updateVoiceNameField(TextToSpeechSettingsData.VOICES.map { it.name })
 
         (accentField.editText as AutoCompleteTextView).setOnItemClickListener { adapterView, view, position, id ->
             voiceNameAccentFilter = ((view as TextView).text as String)
 
-            val accents = TextToSpeechSettingsData.ACCENTS
-
             when(voiceNameAccentFilter) {
-                accents[0].value -> voiceNameAccentFilter = accents[0].name
-                accents[1].value -> voiceNameAccentFilter = accents[1].name
-                accents[2].value -> voiceNameAccentFilter = accents[2].name
+                accentItems[0] -> voiceNameAccentFilter = accentItems[0]
+                accentItems[1] -> voiceNameAccentFilter = accentItems[1]
+                accentItems[2] -> voiceNameAccentFilter = accentItems[2]
             }
 
-            val newVoiceNameItems = voiceNameItems.filter {
-                if (voiceNameGenderFilter.length > 0) {
-                    it.contains(voiceNameAccentFilter) && it.endsWith(voiceNameGenderFilter[0])
-                } else {
-                    it.contains(voiceNameAccentFilter)
-                }
-            }
+            val newVoiceItems = filterVoices()
 
-            updateVoiceNameField(newVoiceNameItems)
+            updateVoiceNameField(newVoiceItems.map { it.name })
         }
 
         (genderField.editText as AutoCompleteTextView).setOnItemClickListener { adapterView, view, position, id ->
             voiceNameGenderFilter = ((view as TextView).text as String)
 
-            val genders = TextToSpeechSettingsData.GENDERS
-
             when(voiceNameGenderFilter) {
-                genders[0].value -> voiceNameGenderFilter = genders[0].name
-                genders[1].value -> voiceNameGenderFilter = genders[1].name
-                genders[2].value -> voiceNameGenderFilter = genders[2].name
+                genderItems[0] -> voiceNameGenderFilter = genderItems[0]
+                genderItems[1] -> voiceNameGenderFilter = genderItems[1]
+                genderItems[2] -> voiceNameGenderFilter = genderItems[2]
             }
 
-            val newVoiceNameItems = voiceNameItems.filter {
-                it.endsWith(voiceNameGenderFilter[0]) && it.contains(voiceNameAccentFilter)
-            }
+            val newVoiceNameItems = filterVoices()
 
-            updateVoiceNameField(newVoiceNameItems)
+            updateVoiceNameField(newVoiceNameItems.map { it.name})
         }
     }
 
@@ -85,5 +73,15 @@ class SettingsActivity : AppCompatActivity() {
         val voiceNameAdapter = ArrayAdapter(this, R.layout.item, voiceNameItems)
         (voiceNameField.editText as? AutoCompleteTextView)?.setAdapter(voiceNameAdapter)
         (voiceNameField.editText as? AutoCompleteTextView)?.setText("")
+    }
+
+    fun filterVoices(): List<Voice> {
+        return TextToSpeechSettingsData.VOICES.filter {
+            if (voiceNameGenderFilter.equals("")) {
+                it.languageCode.contains(voiceNameAccentFilter)
+            } else {
+                it.languageCode.contains(voiceNameAccentFilter) && it.ssmlGender.contains(Regex("^" + voiceNameGenderFilter + "$"))
+            }
+        }
     }
 }
--- app/src/main/java/com/example/android/fluentspeak/TextToSpeechSettingsData.kt
+++ app/src/main/java/com/example/android/fluentspeak/TextToSpeechSettingsData.kt
@@ -1,53 +1,32 @@
 package com.example.android.fluentspeak
 
-data class Accent(
-    val name: String, val value: String
-)
-
-data class Gender(
-    val name: String, val value: String
-)
-
-data class VoiceName(
-    val name: String, val value: String
-)
+import com.example.android.fluentspeak.network.Voice
 
 class TextToSpeechSettingsData {
     companion object {
-        val ACCENTS = listOf<Accent>(
-            Accent("AU", "Australian"),
-            Accent("IN", "Indian"),
-            Accent("GB", "Great Britain")
-        )
-
-        val GENDERS = listOf<Gender>(
-            Gender("MALE", "Male"),
-            Gender("FEMALE", "Female")
-        )
-
-        val VOICE_NAMES = listOf<VoiceName>(
-            VoiceName("en-AU-Neural2-A", "en-AU-Neural2-A-F"),
-            VoiceName("en-AU-Neural2-B", "en-AU-Neural2-B-M"),
-            VoiceName("en-AU-Neural2-C", "en-AU-Neural2-C-F"),
-            VoiceName("en-AU-Neural2-D", "en-AU-Neural2-D-M"),
-            VoiceName("en-AU-Standard-A", "en-AU-Standard-A-F"),
-            VoiceName("en-AU-Standard-B", "en-AU-Standard-B-M"),
-            VoiceName("en-AU-Standard-C", "en-AU-Standard-C-F"),
-            VoiceName("en-AU-Standard-D", "en-AU-Standard-D-M"),
-            VoiceName("en-IN-Standard-A", "en-IN-Standard-A-F"),
-            VoiceName("en-IN-Standard-B", "en-IN-Standard-B-M"),
-            VoiceName("en-IN-Standard-C", "en-IN-Standard-C-M"),
-            VoiceName("en-IN-Standard-D", "en-IN-Standard-D-F"),
-            VoiceName("en-GB-Neural2-A", "en-GB-Neural2-A-F"),
-            VoiceName("en-GB-Neural2-B", "en-GB-Neural2-B-M"),
-            VoiceName("en-GB-Neural2-C", "en-GB-Neural2-C-F"),
-            VoiceName("en-GB-Neural2-D", "en-GB-Neural2-D-M"),
-            VoiceName("en-GB-Neural2-F", "en-GB-Neural2-F-F"),
-            VoiceName("en-GB-Standard-A", "en-GB-Standard-A-F"),
-            VoiceName("en-GB-Standard-B", "en-GB-Standard-B-M"),
-            VoiceName("en-GB-Standard-C", "en-GB-Standard-C-F"),
-            VoiceName("en-GB-Standard-D", "en-GB-Standard-D-M"),
-            VoiceName("en-GB-Standard-F", "en-GB-Standard-F-F"),
+        val VOICES = listOf<Voice>(
+            Voice("en-AU", "en-AU-Neural2-A", "FEMALE"),
+            Voice("en-AU", "en-AU-Neural2-B", "MALE"),
+            Voice("en-AU", "en-AU-Neural2-C", "FEMALE"),
+            Voice("en-AU", "en-AU-Neural2-D", "MALE"),
+            Voice("en-AU", "en-AU-Standard-A", "FEMALE"),
+            Voice("en-AU", "en-AU-Standard-B", "MALE"),
+            Voice("en-AU", "en-AU-Standard-C", "FEMALE"),
+            Voice("en-AU", "en-AU-Standard-D", "MALE"),
+            Voice("en-IN", "en-IN-Standard-A", "FEMALE"),
+            Voice("en-IN", "en-IN-Standard-B", "MALE"),
+            Voice("en-IN", "en-IN-Standard-C", "MALE"),
+            Voice("en-IN", "en-IN-Standard-D", "FEMALE"),
+            Voice("en-GB", "en-GB-Neural2-A", "FEMALE"),
+            Voice("en-GB", "en-GB-Neural2-B", "MALE"),
+            Voice("en-GB", "en-GB-Neural2-C", "FEMALE"),
+            Voice("en-GB", "en-GB-Neural2-D", "MALE"),
+            Voice("en-GB", "en-GB-Neural2-F", "FEMALE"),
+            Voice("en-GB", "en-GB-Standard-A", "FEMALE"),
+            Voice("en-GB", "en-GB-Standard-B", "MALE"),
+            Voice("en-GB", "en-GB-Standard-C", "FEMALE"),
+            Voice("en-GB", "en-GB-Standard-D", "MALE"),
+            Voice("en-GB", "en-GB-Standard-F", "FEMALE"),
         )
     }
 }
