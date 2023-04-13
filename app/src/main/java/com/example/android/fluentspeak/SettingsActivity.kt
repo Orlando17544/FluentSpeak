fe405abe32b571621bad595b7623373c0d92e205
Orlando Canchola Jimenez <orcj17544@gmail.com>
2023-04-12 19:38:56 -0600

    Added values by default

--- app/src/main/java/com/example/android/fluentspeak/SettingsActivity.kt
+++ app/src/main/java/com/example/android/fluentspeak/SettingsActivity.kt
@@ -1,15 +1,21 @@
 package com.example.android.fluentspeak
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.SharedPreferences
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.android.fluentspeak.databinding.ActivitySettingsBinding
 import com.example.android.fluentspeak.network.Voice
+import com.google.android.material.slider.Slider
 import com.google.android.material.textfield.TextInputLayout
 
 class SettingsActivity : AppCompatActivity() {
@@ -23,10 +29,126 @@ class SettingsActivity : AppCompatActivity() {
     var voiceNameAccentFilter = ""
     var voiceNameGenderFilter = ""
 
+    lateinit var sharedPref: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_settings)
 
+        sharedPref = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
+
+        /*
+        getValueFromSharedPreferences(resources.getFraction(R.fraction.whisper_temperature_default_value, 1, 1), getString(R.string.whisper_temperature_key), binding.whisperTemperatureSlider)
+
+        getValueFromSharedPreferences(resources.getFraction(R.fraction.chat_gpt_temperature_default_value, 1, 1), getString(R.string.chat_gpt_temperature_key), binding.chatGptTemperatureSlider)
+        getValueFromSharedPreferences(resources.getInteger(R.integer.chat_gpt_max_tokens_default_value), getString(R.string.chat_gpt_max_tokens_key), binding.chatGptMaxTokensField.editText)
+        getValueFromSharedPreferences(resources.getFraction(R.fraction.chat_gpt_presence_penalty_default_value, 1, 1), getString(R.string.chat_gpt_presence_penalty_key), binding.chatGptPresencePenaltySlider)
+
+        getValueFromSharedPreferences(resources.getFraction(R.fraction.chat_gpt_frecuency_penalty_default_value, 1, 1), getString(R.string.chat_gpt_frecuency_penalty_key), binding.chatGptFrecuencyPenaltySlider)
+
+        getValueFromSharedPreferences(resources.getString(R.string.text_to_speech_accent_default_value), getString(R.string.text_to_speech_accent_key), binding.textToSpeechAccent)
+        getValueFromSharedPreferences(resources.getString(R.string.text_to_speech_gender_default_value), getString(R.string.text_to_speech_gender_key), binding.textToSpeechGender)
+        getValueFromSharedPreferences(resources.getString(R.string.text_to_speech_voice_name_default_value), getString(R.string.text_to_speech_voice_name_key), binding.textToSpeechVoiceName)*/
+
+        setupListeners()
+
+        voiceNameField = binding.textToSpeechVoiceNameField
+        accentField = binding.textToSpeechAccentField
+        genderField = binding.textToSpeechGenderField
+
+        val accentItems = TextToSpeechSettingsData.VOICES.map { it.languageCode }.distinct()
+        val accentAdapter = ArrayAdapter(this, R.layout.item, accentItems)
+        (accentField.editText as? AutoCompleteTextView)?.setAdapter(accentAdapter)
+
+        val genderItems = TextToSpeechSettingsData.VOICES.map { it.ssmlGender }.distinct()
+        val genderAdapter = ArrayAdapter(this, R.layout.item, genderItems)
+        (genderField.editText as? AutoCompleteTextView)?.setAdapter(genderAdapter)
+
+        updateVoiceNameField(TextToSpeechSettingsData.VOICES.map { it.name })
+
+        (accentField.editText as AutoCompleteTextView).setOnItemClickListener { adapterView, view, position, id ->
+            voiceNameAccentFilter = ((view as TextView).text as String)
+
+            val newVoiceItems = filterVoices()
+
+            updateVoiceNameField(newVoiceItems.map { it.name })
+        }
+
+        (genderField.editText as AutoCompleteTextView).setOnItemClickListener { adapterView, view, position, id ->
+            voiceNameGenderFilter = ((view as TextView).text as String)
+
+            val newVoiceNameItems = filterVoices()
+
+            updateVoiceNameField(newVoiceNameItems.map { it.name})
+        }
+
+        binding.save.setOnClickListener {
+            saveValueToSharedPreferences(getString(R.string.whisper_temperature_key), binding.whisperTemperatureSlider.value)
+
+            saveValueToSharedPreferences(getString(R.string.chat_gpt_temperature_key), binding.chatGptTemperatureSlider.value)
+            saveValueToSharedPreferences(getString(R.string.chat_gpt_max_tokens_key), binding.chatGptMaxTokensField.editText?.text.toString())
+            saveValueToSharedPreferences(getString(R.string.chat_gpt_presence_penalty_key), binding.chatGptPresencePenaltySlider.value)
+            saveValueToSharedPreferences(getString(R.string.chat_gpt_frecuency_penalty_key), binding.chatGptFrecuencyPenaltySlider.value)
+
+            saveValueToSharedPreferences(getString(R.string.text_to_speech_accent_key), binding.textToSpeechAccentField.editText?.text.toString())
+            saveValueToSharedPreferences(getString(R.string.text_to_speech_gender_key), binding.textToSpeechGenderField.editText?.text.toString())
+            saveValueToSharedPreferences(getString(R.string.text_to_speech_voice_name_key), binding.textToSpeechVoiceNameField.editText?.text.toString())
+        }
+    }
+
+    fun saveValueToSharedPreferences(key: String, value: Any) {
+        if (value.equals("")) {
+            return
+        }
+
+        if (value is Float) {
+            with (sharedPref.edit()) {
+                putFloat(key, value)
+                apply()
+            }
+        } else if (value is Int) {
+            with (sharedPref.edit()) {
+                putInt(key, value)
+                apply()
+            }
+        } else if (value is String) {
+            with (sharedPref.edit()) {
+                putString(key, value)
+                apply()
+            }
+        }
+    }
+
+    fun updateVoiceNameField(voiceNameItems: List<String>) {
+        val voiceNameAdapter = ArrayAdapter(this, R.layout.item, voiceNameItems)
+        (voiceNameField.editText as? AutoCompleteTextView)?.setAdapter(voiceNameAdapter)
+        (voiceNameField.editText as? AutoCompleteTextView)?.setText("")
+    }
+
+    fun filterVoices(): List<Voice> {
+        return TextToSpeechSettingsData.VOICES.filter {
+            if (voiceNameGenderFilter.equals("")) {
+                it.languageCode.contains(voiceNameAccentFilter)
+            } else {
+                it.languageCode.contains(voiceNameAccentFilter) && it.ssmlGender.contains(Regex("^" + voiceNameGenderFilter + "$"))
+            }
+        }
+    }
+
+    fun getValueFromSharedPreferences(defaultValue: Any, key: String, view: View?) {
+        if (defaultValue is Float) {
+            val savedValue = sharedPref.getFloat(key, defaultValue)
+            (view as Slider).value = savedValue
+        } else if (defaultValue is Int) {
+            val savedValue = sharedPref.getInt(key, defaultValue)
+            (view as EditText).setText(savedValue.toString())
+        } else if (defaultValue is String) {
+            val savedValue = sharedPref.getString(key, defaultValue)
+            (view as EditText).setText(savedValue)
+        }
+    }
+
+    fun setupListeners() {
         binding.whisperTemperatureImage.setOnClickListener {
             Toast.makeText(this, "The sampling temperature, between 0 and 1. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic. If set to 0, the model will use log probability to automatically increase the temperature until certain thresholds are hit.", Toast.LENGTH_LONG).show()
         }
@@ -61,95 +183,6 @@ class SettingsActivity : AppCompatActivity() {
 
         binding.textToSpeechVoiceNameImage.setOnClickListener {
             Toast.makeText(this, "The voice name that match with the accent and gender previously selected", Toast.LENGTH_LONG).show()
-        }
-
-        voiceNameField = binding.textToSpeechVoiceNameField
-        accentField = binding.textToSpeechAccentField
-        genderField = binding.textToSpeechGenderField
-
-        val accentItems = TextToSpeechSettingsData.VOICES.map { it.languageCode }.distinct()
-        val accentAdapter = ArrayAdapter(this, R.layout.item, accentItems)
-        (accentField.editText as? AutoCompleteTextView)?.setAdapter(accentAdapter)
-
-        val genderItems = TextToSpeechSettingsData.VOICES.map { it.ssmlGender }.distinct()
-        val genderAdapter = ArrayAdapter(this, R.layout.item, genderItems)
-        (genderField.editText as? AutoCompleteTextView)?.setAdapter(genderAdapter)
-
-        updateVoiceNameField(TextToSpeechSettingsData.VOICES.map { it.name })
-
-        (accentField.editText as AutoCompleteTextView).setOnItemClickListener { adapterView, view, position, id ->
-            voiceNameAccentFilter = ((view as TextView).text as String)
-
-            val newVoiceItems = filterVoices()
-
-            updateVoiceNameField(newVoiceItems.map { it.name })
-        }
-
-        (genderField.editText as AutoCompleteTextView).setOnItemClickListener { adapterView, view, position, id ->
-            voiceNameGenderFilter = ((view as TextView).text as String)
-
-            val newVoiceNameItems = filterVoices()
-
-            updateVoiceNameField(newVoiceNameItems.map { it.name})
-        }
-
-        binding.save.setOnClickListener {
-            saveValueToSharedPreferences(getString(R.string.whisper_temperature), binding.whisperTemperatureSlider.value.toString())
-
-            saveValueToSharedPreferences(getString(R.string.chat_gpt_temperature), binding.chatGptTemperatureSlider.value.toString())
-            saveValueToSharedPreferences(getString(R.string.chat_gpt_max_tokens), binding.chatGptMaxTokensField.editText?.text.toString())
-            saveValueToSharedPreferences(getString(R.string.chat_gpt_presence_penalty), binding.chatGptPresencePenaltySlider.value.toString())
-            saveValueToSharedPreferences(getString(R.string.chat_gpt_frecuency_penalty), binding.chatGptFrecuencyPenaltySlider.value.toString())
-
-            saveValueToSharedPreferences(getString(R.string.text_to_speech_accent), binding.textToSpeechAccentField.editText?.text.toString())
-            saveValueToSharedPreferences(getString(R.string.text_to_speech_gender), binding.textToSpeechGenderField.editText?.text.toString())
-            saveValueToSharedPreferences(getString(R.string.text_to_speech_voice_name), binding.textToSpeechVoiceNameField.editText?.text.toString())
-        }
-    }
-
-    fun saveValueToSharedPreferences(key: String, value: String) {
-        if (value.equals("")) {
-            return
-        }
-
-        if (key.equals(getString(R.string.chat_gpt_max_tokens)) && !value.equals(Regex("^\\d+$"))) {
-            return
-        }
-
-        if (value.equals(Regex("^\\d+\\.\\d+$"))) {
-            val sharedPref = getPreferences(Context.MODE_PRIVATE) ?: return
-            with (sharedPref.edit()) {
-                putFloat(key, value.toFloat())
-                apply()
-            }
-        } else if (value.equals(Regex("^\\d+$"))) {
-            val sharedPref = getPreferences(Context.MODE_PRIVATE) ?: return
-            with (sharedPref.edit()) {
-                putInt(key, value.toInt())
-                apply()
-            }
-        } else {
-            val sharedPref = getPreferences(Context.MODE_PRIVATE) ?: return
-            with (sharedPref.edit()) {
-                putString(key, value)
-                apply()
-            }
-        }
-    }
-
-    fun updateVoiceNameField(voiceNameItems: List<String>) {
-        val voiceNameAdapter = ArrayAdapter(this, R.layout.item, voiceNameItems)
-        (voiceNameField.editText as? AutoCompleteTextView)?.setAdapter(voiceNameAdapter)
-        (voiceNameField.editText as? AutoCompleteTextView)?.setText("")
-    }
-
-    fun filterVoices(): List<Voice> {
-        return TextToSpeechSettingsData.VOICES.filter {
-            if (voiceNameGenderFilter.equals("")) {
-                it.languageCode.contains(voiceNameAccentFilter)
-            } else {
-                it.languageCode.contains(voiceNameAccentFilter) && it.ssmlGender.contains(Regex("^" + voiceNameGenderFilter + "$"))
-            }
         }
     }
 }
