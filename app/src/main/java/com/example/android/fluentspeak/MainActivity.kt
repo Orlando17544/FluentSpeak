3a7f8076fc30c4ecaa649e5fa5a0cf7d39634edb
Orlando Canchola Jimenez <orcj17544@gmail.com>
2023-04-16 20:23:58 -0600

    SharedPreferences are initialized with default values at start

--- app/src/main/java/com/example/android/fluentspeak/MainActivity.kt
+++ app/src/main/java/com/example/android/fluentspeak/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.android.fluentspeak
 
 import android.app.Activity
+import android.content.Context
+import android.content.SharedPreferences
 import android.content.res.AssetFileDescriptor
 import android.net.Uri
 import android.os.Bundle
@@ -9,6 +11,7 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -16,6 +19,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.slider.Slider
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import org.tensorflow.lite.Interpreter
@@ -28,9 +32,19 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var sharedPref: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sharedPref = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
+
+        val isFirstStart = sharedPref.getBoolean(getString(R.string.first_start_key), resources.getBoolean(R.bool.first_start_default_value))
+
+        if(isFirstStart) {
+            saveDefaultValuesToSharedPreferences()
+        }
 
         val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
@@ -61,5 +75,24 @@ class MainActivity : AppCompatActivity() {
         }
 
         bottomNavigation.selectedItemId = R.id.page_1
+    }
+
+    fun saveDefaultValuesToSharedPreferences() {
+        with (sharedPref.edit()) {
+            putFloat(getString(R.string.whisper_temperature_key), resources.getFraction(R.fraction.whisper_temperature_default_value, 1, 1))
+
+            putFloat(getString(R.string.chat_gpt_temperature_key), resources.getFraction(R.fraction.chat_gpt_temperature_default_value, 1, 1))
+            putInt(getString(R.string.chat_gpt_max_tokens_key), resources.getInteger(R.integer.chat_gpt_max_tokens_default_value))
+            putFloat(getString(R.string.chat_gpt_presence_penalty_key), resources.getFraction(R.fraction.chat_gpt_presence_penalty_default_value, 1, 1))
+            putFloat(getString(R.string.chat_gpt_frecuency_penalty_key), resources.getFraction(R.fraction.chat_gpt_frecuency_penalty_default_value, 1, 1))
+
+            putString(getString(R.string.text_to_speech_accent_key), resources.getString(R.string.text_to_speech_accent_default_value))
+            putString(getString(R.string.text_to_speech_gender_key), resources.getString(R.string.text_to_speech_gender_default_value))
+            putString(getString(R.string.text_to_speech_voice_name_key), resources.getString(R.string.text_to_speech_voice_name_default_value))
+
+            putBoolean(getString(R.string.first_start_key), false)
+
+            apply()
+        }
     }
 }
